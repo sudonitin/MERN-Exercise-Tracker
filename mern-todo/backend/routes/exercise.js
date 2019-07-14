e4d11b5b680ddb6e5cbe7f955518a7455aa8d351
globefire <sahun5774@gmail.com>
2019-07-15 03:03:08 +0530

    CRUD operations ✅

--- mern-todo/backend/routes/exercise.js
+++ mern-todo/backend/routes/exercise.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
-let Exercise = require('../models/exercise.model');
+let Exercise = require('../models/exercise.model'); //creating a instance of exercise model to refer to the structure and use it to find data from DB and insert into DB
 
+
+//get all exercises
 router.route('/').get((req, res) => {
     Exercise.find()
         .then(exercise => res.json(exercise))
         .catch(err => res.status(400).json('Error: '+err));
 });
 
+
+//add a new exercise
 router.route('/add').post((req, res) => {
     const username = req.body.username;
     const description = req.body.description;
@@ -25,5 +29,34 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: '+err));
 });
 
+//find a single exercise
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: '+err));
+})
+
+//delete an exercise
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Exercise Deleted!!'))
+        .catch(err => res.status(400).json('Error: '+err));
+});
+
+//update an exercise
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+
+        exercise.save()
+            .then(() => res.json('Exercise Updated!!'+ exercise))
+            .catch(err => res.status(400).json('Error: '+err));
+    })
+    .catch(err => res.status(400).json('Error: '+err));
+});
 module.exports = (router);
 
