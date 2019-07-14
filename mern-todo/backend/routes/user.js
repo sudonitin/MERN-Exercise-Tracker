cf4c59f6494c1cb568869bdc2a09af2086aceb5c
globefire <sahun5774@gmail.com>
2019-07-14 20:49:31 +0530

    routes for exercise added

--- mern-todo/backend/routes/user.js
+++ mern-todo/backend/routes/user.js
@@ -3,14 +3,14 @@ let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
     User.find() //a mongoose fn which returns all the users
-    .then(users => res.json(users))
+    .then(user => res.json(user))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req,res) => {
     const username = req.body.username;
     const newUser = new User({username});
-    newUser.save()
+    newUser.save() // mongoose fn used to add data to DB
         .then(() => res.json('User added'))
         .catch(err => res.status(400).json('Error: '+ err));
 });
