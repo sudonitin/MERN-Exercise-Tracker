4a50b7aca790c64f934b3b78ff8474e1c3408872
globefire <sahun5774@gmail.com>
2019-07-15 17:24:49 +0530

    react router script added

--- mern-todo/src/App.js
+++ mern-todo/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // we create a router element for each route of application, inside these router elements we have defined components. These components are created separately.
+    <Router> 
+      <Navbar />
+      <br/>
+    <Route path="/" exact component={ExercisesList} />
+    <Route path="/edit/:id" component={EditExercise} />
+    <Route path="/create" component={CreateExercise} />
+    <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
